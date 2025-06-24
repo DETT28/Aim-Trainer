b0e90e11f64e0a374acef28e826c04315902b446
Detto! <165959776+DETT28@users.noreply.github.com>
2025-06-24 16:31:15 -0400

    Add files via upload

---
+++ Scripts/player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody3D
+
+const SENSITIVITY = 0.001
+
+@onready var camera = $CameraController/Camera3D
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * SENSITIVITY)
+		camera.rotate_x(-event.relative.y * SENSITIVITY)
+		camera.rotation.x = clamp(camera.rotation.x, -PI/2, PI/2)
